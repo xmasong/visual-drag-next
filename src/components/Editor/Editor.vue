ea1348bdd6be29fc4703c7fa3546450fc2a84946
xmasong <544479929@qq.com>
2023-01-08 10:06:40 +0800

    feat: the dray delay is due to css transition 0.1s

--- src/components/Editor/Editor.vue
+++ src/components/Editor/Editor.vue
@@ -1,61 +1,25 @@
 <template>
   <div class="editor" id="editor">
-    <component
+    <Shape
       v-for="(item, index) in componentData"
-      :key="`${item.id}+${fresh}`"
-      :is="item.component"
+      :key="`${item.id}`"
+      :element="item"
+      :index="index"
       :style="getShapeStyle(item.style)"
-      :propValue="item.propValue"
-      @mousedown="handleMouseDown($event, item, index)"
-    />
+    >
+      <component :is="item.component" :propValue="item.propValue" />
+    </Shape>
   </div>
 </template>
 <script setup lang="ts">
 import { useComponent } from "@/stores/canvas";
-import type { Pos } from "@/types";
 import { storeToRefs } from "pinia";
 import { getShapeStyle } from "@/utils/style";
-import { ref } from "vue";
+import Shape from "../Shape.vue";
 
 const componentStore = useComponent();
-const { setCurComponent, setShapeStyle } = componentStore;
+// const { setCurComponent, setShapeStyle } = componentStore;
 const { componentData } = storeToRefs(componentStore);
-const fresh = ref(0);
-
-function handleMouseDown(e: MouseEvent, element, index) {
-  e.stopPropagation();
-
-  setCurComponent({
-    component: element,
-    index,
-  });
-
-  const pos: Pos = { ...element.style };
-  const startY = e.clientY;
-  const startX = e.clientX;
-  // 如果直接修改属性，值的类型会变为字符串，所以要转为数值型
-  const startTop = Number(pos.top);
-  const startLeft = Number(pos.left);
-
-  const move = (moveEvent: MouseEvent) => {
-    fresh.value++;
-    const currX = moveEvent.clientX;
-    const currY = moveEvent.clientY;
-
-    pos.top = currY - startY + startTop;
-    pos.left = currX - startX + startLeft;
-    // 修改当前组件样式
-    setShapeStyle(pos);
-  };
-
-  const up = () => {
-    document.removeEventListener("mousemove", move);
-    document.removeEventListener("mouseup", up);
-  };
-
-  document.addEventListener("mousemove", move);
-  document.addEventListener("mouseup", up);
-}
 </script>
 <style lang="scss">
 .editor {
---
+++ src/components/Shape.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="shape" @mousedown="handleMouseDownOnShape">
+    <slot></slot>
+  </div>
+</template>
+<script setup lang="ts">
+import { useComponent } from "@/stores/canvas";
+import type { Pos, Component } from "@/types";
+import { ref } from "vue";
+
+const componentStore = useComponent();
+const { setCurComponent, setShapeStyle } = componentStore;
+const fresh = ref(0);
+const { element, index } = defineProps({
+  element: {
+    required: true,
+    type: Object,
+    default: () => {},
+  },
+  index: {
+    required: true,
+    type: Number,
+    default: 0,
+  },
+});
+
+function handleMouseDownOnShape(e: MouseEvent) {
+  e.stopPropagation();
+
+  setCurComponent({
+    component: element as Component,
+    index,
+  });
+
+  const pos: Pos = { ...element.style };
+  const startY = e.clientY;
+  const startX = e.clientX;
+  // 如果直接修改属性，值的类型会变为字符串，所以要转为数值型
+  const startTop = Number(pos.top);
+  const startLeft = Number(pos.left);
+
+  const move = (moveEvent: MouseEvent) => {
+    fresh.value++;
+    const currX = moveEvent.clientX;
+    const currY = moveEvent.clientY;
+
+    pos.top = currY - startY + startTop;
+    pos.left = currX - startX + startLeft;
+    // 修改当前组件样式
+    setShapeStyle(pos);
+  };
+
+  const up = () => {
+    document.removeEventListener("mousemove", move);
+    document.removeEventListener("mouseup", up);
+  };
+
+  document.addEventListener("mousemove", move);
+  document.addEventListener("mouseup", up);
+}
+</script>
+<style lang="scss">
+.shape {
+  position: absolute;
+}
+</style>
