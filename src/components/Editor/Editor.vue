5d3fe50f4aecd93ac0632d5d5fb04b993aba30df
xmasong <544479929@qq.com>
2023-01-11 19:05:52 +0800

    feat: add handleContextMenu event

--- src/components/Editor/Editor.vue
+++ src/components/Editor/Editor.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="editor" id="editor">
+  <div class="editor" id="editor" @contextmenu="handleContextMenu">
     <Shape
       v-for="(item, index) in componentData"
       :key="`${item.id}`"
@@ -18,6 +18,27 @@ import Shape from "../Shape.vue";
 const componentStore = useComponent();
 // const { setCurComponent, setShapeStyle } = componentStore;
 const { componentData } = storeToRefs(componentStore);
+
+function handleContextMenu(e) {
+  e.stopPropagation();
+  e.preventDefault();
+
+  // 计算菜单相对于编辑器的位移
+  let target = e.target;
+  let top = e.offsetY;
+  let left = e.offsetX;
+  while (target instanceof SVGElement) {
+    target = target.parentNode;
+  }
+
+  while (!target.className.includes("editor")) {
+    left += target.offsetLeft;
+    top += target.offsetTop;
+    target = target.parentNode;
+  }
+
+  // this.$store.commit("showContextMenu", { top, left });
+}
 </script>
 <style lang="scss">
 .editor {
