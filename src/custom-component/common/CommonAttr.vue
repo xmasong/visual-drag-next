a207158cc7e1bb78b4ecf5f3c900b24e3835f850
xmasong <544479929@qq.com>
2023-02-01 18:47:22 +0800

    feat: init common attr

---
+++ src/custom-component/common/CommonAttr.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="v-common-attr">
+    <el-collapse v-model="activeName" accordion @change="onChange">
+      <el-collapse-item title="通用样式" name="style">
+        <el-form>
+          <el-form-item
+            v-for="({ key, label }, index) in styleKeys"
+            :key="index"
+            :label="label"
+          >
+            <el-input v-model.number="curComponent.style[key]" type="number" />
+          </el-form-item>
+        </el-form>
+      </el-collapse-item>
+    </el-collapse>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss">
+.v-common-attr {
+  margin-bottom: 18px;
+}
+</style>
