153a9b99dadb19e4978d8e535286b7d5fa6929fb
xmasong <544479929@qq.com>
2023-02-08 16:45:50 +0800

    feat: init preview

--- src/components/ToolBar.vue
+++ src/components/ToolBar.vue
@@ -4,13 +4,15 @@
       <el-button>JSON</el-button>
       <el-button @click="undo">撤销</el-button>
       <el-button @click="redo">重做</el-button>
+      <el-button @click="preview(false)">预览</el-button>
     </el-row>
   </div>
 </template>
 <script setup lang="ts">
 import { useSnapshot } from "@/stores";
-
 const { undo, redo } = useSnapshot();
+
+function preview(isScreenshot) {}
 </script>
 <style lang="scss">
 .tool-bar {
