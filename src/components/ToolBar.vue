bb0420ca788b4cfc2368136b03329d6f3aa4509b
xmasong <544479929@qq.com>
2023-02-17 11:02:45 +0800

    feat: toolbar save

--- src/components/ToolBar.vue
+++ src/components/ToolBar.vue
@@ -5,7 +5,9 @@
       <el-button @click="undo">撤销</el-button>
       <el-button @click="redo">重做</el-button>
       <el-button @click="preview">预览</el-button>
+      <el-button @click="save">保存</el-button>
     </el-row>
+
     <!-- 预览 -->
     <Preview
       v-if="isShowPreview"
@@ -18,21 +20,30 @@
 import { useComponent, useSnapshot } from "@/stores";
 import { ref } from "vue";
 import Preview from "./Editor/Preview.vue";
+import { ElMessage } from "element-plus";
+import { storeToRefs } from "pinia";
 const { undo, redo } = useSnapshot();
 
 const isShowPreview = ref(false);
 const isScreenshot = ref(false);
-const { setEditMode } = useComponent();
+const componentStore = useComponent();
+const { componentData, canvasStyleData } = storeToRefs(componentStore);
 
 function preview() {
   isScreenshot.value = false;
   isShowPreview.value = true;
-  setEditMode("preview");
+  componentStore.setEditMode("preview");
 }
 
 function handlePreviewChange() {
   isShowPreview.value = false;
-  setEditMode("edit");
+  componentStore.setEditMode("edit");
+}
+
+function save() {
+  localStorage.setItem("canvasData", JSON.stringify(componentData));
+  localStorage.setItem("canvasStyle", JSON.stringify(canvasStyleData));
+  ElMessage.success("保存成功");
 }
 </script>
 <style lang="scss">
