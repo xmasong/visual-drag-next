c1e3e8475da461d11987d932d69a687665d304db
xmasong <544479929@qq.com>
2023-03-13 19:42:35 +0800

    feat: update Picture comp to vue3

--- src/custom-component/Picture/Component.vue
+++ src/custom-component/Picture/Component.vue
@@ -5,5 +5,53 @@
 </template>
 
 <script setup lang="ts">
+import { onMounted, ref } from "vue";
+
 const props = defineProps(["propValue", "element"]);
+const canvasRef = ref();
+const canvasCtx = ref();
+const isFirst = ref(true);
+const img = ref();
+
+function drawImage() {
+  const { width, height } = props.element.style;
+  canvasRef.value.width = width;
+  canvasRef.value.height = height;
+  if (isFirst.value) {
+    isFirst.value = false;
+    img.value = document.createElement("img");
+    img.value.src = props.propValue.url;
+    img.value.onload = () => {
+      canvasCtx.value.drawImage(img.value, 0, 0, width, height);
+      mirrorFlip();
+    };
+  } else {
+    mirrorFlip();
+  }
+}
+
+function mirrorFlip() {
+  const { vertical, horizontal } = props.propValue.flip;
+  const { width, height } = props.element.style;
+  const hvalue = horizontal ? -1 : 1;
+  const vValue = vertical ? -1 : 1;
+
+  // 清除图片
+  canvasCtx.value.clearRect(0, 0, width, height);
+  // 平移图片
+  canvasCtx.value.translate(
+    width / 2 - (width * hvalue) / 2,
+    height / 2 - (height * vValue) / 2
+  );
+  // 对称镜像
+  canvasCtx.value.scale(hvalue, vValue);
+  canvasCtx.value.drawImage(img.value, 0, 0, width, height);
+  // 还原坐标点
+  canvasCtx.value.setTransform(1, 0, 0, 1, 0, 0);
+}
+
+onMounted(() => {
+  canvasCtx.value = canvasRef.value.getContext("2d");
+  drawImage();
+});
 </script>
