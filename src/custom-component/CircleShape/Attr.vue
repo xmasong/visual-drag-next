ec8401dc2f75d9f638342cb0defd49f78998d89b
xmasong <544479929@qq.com>
2023-03-26 08:50:43 +0800

    feat: init circle shape

---
+++ src/custom-component/CircleShape/Attr.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="attr-list">
+    <CommonAttr></CommonAttr>
+    <el-form v-if="curComponent">
+      <el-form-item label="内容">
+        <el-input v-model="curComponent.propValue" type="textarea" :rows="3" />
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script setup lang="ts">
+import CommonAttr from "@/custom-component/common/CommonAttr.vue";
+import { useComponent } from "@/stores";
+import { storeToRefs } from "pinia";
+
+const componentStore = useComponent();
+const { curComponent } = storeToRefs(componentStore);
+</script>
