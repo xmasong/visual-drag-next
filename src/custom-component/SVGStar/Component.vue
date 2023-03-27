fbe9d395c3069fa27ab3338ee15c652399cac45b
xmasong <544479929@qq.com>
2023-03-27 13:04:54 +0800

    feat: svgStar with bg color error

--- src/custom-component/SVGStar/Component.vue
+++ src/custom-component/SVGStar/Component.vue
@@ -13,66 +13,41 @@
   </div>
 </template>
 
-<script>
-import OnEvent from "../../common/OnEvent";
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
 
-export default {
-  extends: OnEvent,
-  props: {
-    propValue: {
-      type: String,
-      required: true,
-      default: "",
-    },
-    element: {
-      type: Object,
-      default: () => {},
-    },
-  },
-  data() {
-    return {
-      points: "",
-    };
-  },
-  watch: {
-    "element.style.width": function () {
-      this.draw();
-    },
-    "element.style.height": function () {
-      this.draw();
-    },
-  },
-  mounted() {
-    this.draw();
-  },
-  methods: {
-    draw() {
-      const { width, height } = this.element.style;
-      this.drawPolygon(width, height);
-    },
+const props = defineProps(["propValue", "element"]);
+const points = ref("");
 
-    drawPolygon(width, height) {
-      // 五角星十个坐标点的比例集合
-      const points = [
-        [0.5, 0],
-        [0.625, 0.375],
-        [1, 0.375],
-        [0.75, 0.625],
-        [0.875, 1],
-        [0.5, 0.75],
-        [0.125, 1],
-        [0.25, 0.625],
-        [0, 0.375],
-        [0.375, 0.375],
-      ];
+function draw() {
+  const { width, height } = props.element.style;
+  drawPolygon(width, height);
+}
 
-      const coordinatePoints = points.map(
-        (point) => width * point[0] + " " + height * point[1]
-      );
-      this.points = coordinatePoints.toString();
-    },
-  },
-};
+function drawPolygon(width, height) {
+  // 五角星十个坐标点的比例集合
+  const pointsScale = [
+    [0.5, 0],
+    [0.625, 0.375],
+    [1, 0.375],
+    [0.75, 0.625],
+    [0.875, 1],
+    [0.5, 0.75],
+    [0.125, 1],
+    [0.25, 0.625],
+    [0, 0.375],
+    [0.375, 0.375],
+  ];
+
+  const coordinatePoints = pointsScale.map(
+    (point) => width * point[0] + " " + height * point[1]
+  );
+  points.value = coordinatePoints.toString();
+}
+
+onMounted(() => {
+  draw();
+});
 </script>
 
 <style lang="scss" scoped>
